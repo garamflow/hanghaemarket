05a6e6bbdb0552858aa6131314f25807a2a55e5b
garamflow <garamflow@gmail.com>
2024-09-09 10:11:31 +0900

    chore: 프로젝트 생성 완료

---
+++ src/test/java/com/github/garamflow/hanghaemarket/HanghaemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.garamflow.hanghaemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaemarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
